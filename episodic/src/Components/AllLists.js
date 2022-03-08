6779c29f244b3ecc381a7db023b232dc5d4aa145
KyleenFerris <kyleen@uoguelph.ca>
2022-03-07 20:42:32 -0500

    Add AllLists functionality

--- episodic/src/Components/AllLists.js
+++ episodic/src/Components/AllLists.js
@@ -6,31 +6,56 @@ import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 
+class AllLists extends React.Component {
 
-const allLists = [
-    {
-      name: "listName1"
-    },
+  constructor(props) {
+    super(props);
 
-    {
-      name: "listName2"
-    },
-    {
-      name: "listName3"
-    },
-    {
-      name: "listName4"
-    },
-]
-export default function AllLists() {
-  return (
-    <Box container>
-      <Typography>Lists</Typography>
-      <List>
-      {allLists.map((item) => (
-        <ListItem key={item.name}>{item.name}</ListItem>
-      ))}
-      </List>
-    </Box>
-  );
+    this.state = {
+      allLists: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUserLists();
+  }
+
+  getUserLists = async e => {
+    //e.preventDefault();
+    const response = await fetch("/api/v1/lists/get/all");
+    const body = await response.json();
+    //const listMap = body.lists.map((list) => {list.name});
+    let listNames = [];
+    body.lists.map((list) => listNames.push({name: list.name}))
+    const listMap = [
+      {
+        name: "listName1"
+      },
+
+      {
+        name: "listName2"
+      },
+      {
+        name: "listName3"
+      },
+      {
+        name: "listName4"
+      },
+    ]
+    this.setState({ allLists: listNames })
+  };
+  render() {
+    return (
+      <Box container>
+        <Typography>Lists</Typography>
+        <List>
+          {this.state.allLists.map((item) => (
+            <ListItem key={item.name}>{item.name}</ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+  }
 }
+
+export default AllLists;
