8b70129c94274baadbde316133c4ab3723561918
KyleenFerris <kyleen@uoguelph.ca>
2022-03-13 22:51:59 -0400

    use env

--- episodic/src/repositories/mysql.js
+++ episodic/src/repositories/mysql.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql2');
 
 const pool =  mysql.createConnection({
-    host: "episodic.mysql.database.azure.com",
-    user: "hohenade",
-    password: "[q#rPZMd[~4WF(9t",
-    database: "episodic",
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
     port: 3306
 });
 
