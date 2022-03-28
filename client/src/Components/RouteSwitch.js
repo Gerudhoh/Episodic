31758a79b08d879484d7e414799c2b896ac75637
Gerudhoh <hohenade@uoguelph.ca>
2022-03-28 12:40:45 -0400

    Adding functionality for playing random episodes

--- client/src/Components/RouteSwitch.js
+++ client/src/Components/RouteSwitch.js
@@ -14,6 +14,7 @@ import EpisodeInfoPage from './EpisodeInfoPage';
 import TrendingPage from './TrendingPage';
 import SingleListView from './SingleListView';
 import ProfileListView from './ProfileListView';
+import RandomEpPage from './RandomEpPage';
 
 class RouteSwitch extends React.Component {
   constructor(props) {
@@ -48,6 +49,7 @@ class RouteSwitch extends React.Component {
                 <Route path="/episodeinfo/*" element={<EpisodeInfoPage changeAuth={this.changeAuth} checkAuthData={this.checkAuthData}  changeRemoveAuth={this.changeRemoveAuth} className="tabContent" userId={this.props.userId}/>}/>
                 <Route path="/info/*" element={<PodcastInfoPage changeAuth={this.changeAuth} checkAuthData={this.checkAuthData}  changeRemoveAuth={this.changeRemoveAuth} className="tabContent" userId={this.props.userId}/>}/>
                 <Route path="/trending" element={<TrendingPage changeAuth={this.changeAuth} checkAuthData={this.checkAuthData}  changeRemoveAuth={this.changeRemoveAuth} className="tabContent"/>}/>
+                <Route path="/randomep" element={<RandomEpPage changeAuth={this.changeAuth} checkAuthData={this.checkAuthData}  changeRemoveAuth={this.changeRemoveAuth} className="tabContent"/>}/>
 
                 {/* Logged In routes */}
                 <Route exact path="/" element={<HomePage changeAuth={this.changeAuth} changeRemoveAuth={this.changeRemoveAuth} className="tabContent" auth={this.props.auth} email={this.props.email} username={this.props.username} userId={this.props.userId}/>}/>
