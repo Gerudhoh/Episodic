af53a6df9af671a4cef6edb3e24ccd5e84dd2da4
Mackenzie Quigley <mquigl01@uoguelph.ca>
2022-04-05 14:20:15 -0400

    Fixing size of icons for username to not cut off

--- client/src/Components/ActivityCard.js
+++ client/src/Components/ActivityCard.js
@@ -54,15 +54,14 @@ function MoveListPreview(props) {
 
 function AddEpisodePreview(props) {
   return (
-    <Stack alignItems="flex-start" spacing={2}>
-      
-      <Box sx={{ position: 'relative' }} replace>
+    <Stack alignItems="flex-start" spacing={1}>
+      <Box sx={{ position: 'relative', display: "flex", justifyContent: "center", paddingBottom: "10px" }} replace>
             < img width="100" height="auto"
               src={props.image}
               alt={`${props.reviewText}`}
               loading="lazy"
             />
-        <Typography fontSize={props.fontSize}>{props.reviewText}</Typography><br/>
+        <Typography fontSize={props.fontSize} style={{padding: "10px", paddingTop: "10px"}}>{props.reviewText}</Typography><br/>
       </Box>
     </Stack>
   );
@@ -71,7 +70,6 @@ function AddEpisodePreview(props) {
 
 export default function ActivityCard(props) {
   const size = activityCardStyles[props.activitySize];
-  console.log(props);
 
   const ActivityPreview = (activityType) => {
     if (activityType === 'newList'){
